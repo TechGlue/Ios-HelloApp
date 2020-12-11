7d2fe8189d386089022c0b361fc1ebc839aad607
TechGlue <Garcialuis24@hotmail.com>
2020-12-10 23:10:46 -0800

    Initial Commit

--- HelloApp/ViewController.swift
+++ HelloApp/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    var backgroundColor: UIColor!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        backgroundColor = view.backgroundColor
     }
-
-
+    
+   
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func didTapButton(_ sender: Any) {
+        textLabel.textColor = UIColor.purple
+    }
+    
+    @IBAction func didTapViewButton(_ sender: Any) {
+        view.backgroundColor = UIColor.brown
+    }
+    
+    @IBAction func didTapTextButton(_ sender: Any) {
+        
+       // textLabel.text = "Goodbye ;)"
+        textLabel.text = textField.text
+        textField.text = " "
+        view.endEditing(true)
+    }
+    
+    @IBAction func onResetGesture(_ sender: Any) {
+        textLabel.text = "Hello from Luís!"
+        view.backgroundColor = backgroundColor
+        
+    }
 }
+
 
